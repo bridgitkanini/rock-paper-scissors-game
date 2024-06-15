5f3fb53e653d4b8f1292108de68c6645d12cc643
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-15 12:20:37 +0300

    Create footer bttons with state toggle function for modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [advancedMode, setAdvancedMode] = useState(false);
+  function changeModes() {
+    setAdvancedMode(!advancedMode);
+  }
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-slate-950">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <footer className="footer w-full flex justify-between py-1 px-5 bottom-3 fixed">
+        <div onClick={changeModes} className="modes py-2 px-6 border border-white rounded-lg text-white uppercase">
+          {advancedMode ? "Advanced" : "Normal"} Mode
+        </div>
+        <div className="rules py-2 px-6 border border-white rounded-lg text-white uppercase">Rules</div>
+      </footer>
     </>
   );
 }
