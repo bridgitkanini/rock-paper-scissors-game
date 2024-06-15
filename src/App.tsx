e2ba32c7c675f1f4f7c1a1f454fdca8b1d5c0a07
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-15 14:32:10 +0300

    Add rules popup image to rules button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 import { Header } from "./components/Header";
+import { Rules } from "./components/Rules"
 
 function App() {
   const [advancedMode, setAdvancedMode] = useState(false);
+  const [open, setOpen] = useState(false);
   function changeModes() {
     setAdvancedMode(!advancedMode);
   }
@@ -12,11 +14,24 @@ function App() {
     <>
       <Header advanced={advancedMode} score={0} />
       <footer className="footer w-full flex justify-between py-1 px-5 bottom-3 fixed">
-        <div onClick={changeModes} className="modes py-2 px-6 border border-white rounded-lg text-white uppercase">
+        <div
+          onClick={changeModes}
+          className="modes py-2 px-6 border border-white rounded-lg text-white uppercase"
+        >
           {advancedMode ? "Advanced" : "Normal"} Mode
         </div>
-        <div className="rules py-2 px-6 border border-white rounded-lg text-white uppercase">Rules</div>
+        <div
+          onClick={() => setOpen(!open)}
+          className="rules py-2 px-6 border border-white rounded-lg text-white uppercase"
+        >
+          Rules
+        </div>
       </footer>
+      <Rules
+        open={open}
+        advanced={advancedMode}
+        setopen={() => setOpen(!open)}
+      />
     </>
   );
 }
---
+++ src/components/Rules.tsx
@@ -0,0 +1,45 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { FunctionComponent } from "react";
+import iconclose from "./../../rock-paper-scissors-master/images/icon-close.svg";
+import rulesforadvanced from "./../../rock-paper-scissors-master/images/image-rules-bonus.svg";
+import rulesfornormal from "./../../rock-paper-scissors-master/images/image-rules.svg";
+
+type RulesProps = {
+  advanced: boolean;
+  setopen?: any;
+  open?: boolean;
+};
+
+export const Rules: FunctionComponent<RulesProps> = ({
+  open,
+  advanced,
+  setopen,
+}) => {
+  return (
+    <AnimatePresence>
+      {open && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="absolute top-0 lg:top-[50%] left-0 lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[-50%] w-[100%] lg:w-[40%] h-[100%] lg:h-[65%] bg-white p-5 lg:p-8 max-h-[100%] lg:max-h-[650px] shadow-xl "
+          onClick={() => setopen()}
+        >
+          <div className="flex justify-between items-center text-blue-900 uppercase">
+            <h1>Rules</h1>
+            <img src={iconclose} alt="close" width={100} height={100} className="w-[6%] h-[6%] grid place-items-center " />
+          </div>
+          <div className="rules-img">
+            {advanced ? (
+              <img src={rulesforadvanced} alt="rules for advanced" />
+            ) : (
+              <>
+                <img src={rulesfornormal} alt="rules for normal" />
+              </>
+            )}
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
