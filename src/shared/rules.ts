cd62b5967c26794c6618e6cd51ad6be5e7ca9636
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-21 13:44:18 +0300

    Create game rules array.

---
+++ src/shared/rules.ts
@@ -0,0 +1,23 @@
+export const GameRules = [
+  {
+    value: "rock",
+    beats: ["scissors", "lizard"],
+  },
+  {
+    value: "paper",
+    beats: ["rock", "spock"],
+  },
+  {
+    value: "scissors",
+    beats: ["paper", "lizard"],
+  },
+
+  {
+    value: "lizard",
+    beats: ["spock", "paper"],
+  },
+  {
+    value: "spock",
+    beats: ["scissors", "rock"],
+  },
+];
