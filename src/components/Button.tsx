7df6b445110c886f767fb5df44d5703b06abf57d
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-21 13:07:26 +0300

    Centralise image button on play button.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ type ButtonProps = {
 const Button: FunctionComponent<ButtonProps> = ({ className, children, lid }) => {
   return (
     <motion.div layoutId={lid} className={className}>
-        <div className="top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[80%] h-[80%] bg-white rounded-[50%] grid place-items-center ">{children}</div>
+        <div className="top-[50%] left-[50%] translate-x-[12.5%] translate-y-[12.5%] w-[80%] h-[80%] bg-white rounded-[50%] grid place-items-center">{children}</div>
     </motion.div>
   );
 };
